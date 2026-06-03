744d735be0a4398594fda8630ac8b8d6c9ebaf52
Alexis Koss <alexiskoss@Alexiss-MacBook-Pro.local>
2026-06-03 09:54:13 -0700

    bug: :bug: fix HUD error for displaying Day

--- stray-signals/scenes/hud.gd
+++ stray-signals/scenes/hud.gd
@@ -4,7 +4,11 @@ extends TextureButton
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	focus_mode = Control.FOCUS_NONE
-	$"../../Day/DayText".text = "Day " + str(DayManager.day)
+	DayManager.day_changed.connect(_on_day_changed)
+	_on_day_changed(DayManager.day)
+
+func _on_day_changed(new_day):
+	$"../../Day/DayText".text = "Day " + str(new_day)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
